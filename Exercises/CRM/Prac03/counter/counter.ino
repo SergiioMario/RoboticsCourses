e3ea4c12ead88e7a0ba568cbd43be2902f3e55f0
Mark The Magnificent <mnegretev@gmail.com>
2017-02-14 20:19:26 -0600

    CRM-Prac03 finished (I hope so)

---
+++ Exercises/CRM/Prac03/counter/counter.ino
@@ -0,0 +1,25 @@
+/*
+ * Ejemplo para publicar un entero que se incrementa en uno cada 500 ms
+ */
+#include <ros.h>
+#include <std_msgs/Int32.h>
+
+ros::NodeHandle nh;
+
+std_msgs::Int32 msgCounter;
+ros::Publisher pubCounter("/counter", &msgCounter);
+
+void setup()
+{
+  nh.initNode();
+  nh.advertise(pubCounter);
+  msgCounter.data = 0;
+}
+
+void loop()
+{
+  msgCounter.data++;
+  pubCounter.publish( &msgCounter );
+  nh.spinOnce();
+  delay(500);
+}
